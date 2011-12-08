d6bb4d02f82eb1d5ca251288528aacdc25754afe
Avi Deitcher <avi@deitcher.net>
2011-12-08 11:34:58 +0200

    Fixed closure issue with click events on cells

--- jstreegrid.js
+++ jstreegrid.js
@@ -208,9 +208,11 @@
 							span = isAlreadyGrid ? last.children("span") : $("<span></span>").appendTo(last);
 
 							// create a span inside the div, so we can control what happens in the whole div versus inside just the text/background
-							span.addClass(cl+" "+valClass).css("display","inline-block").html(content).click(function () {
-								$(this).trigger("select_cell.jstree-grid", [{value: val,column: col.header,node: $(this).closest("li"),sourceName: col.value,sourceType: s}]);
-							});
+							span.addClass(cl+" "+valClass).css("display","inline-block").html(content).click((function (val) {
+								return function() {
+									$(this).trigger("select_cell.jstree-grid", [{value: val,column: col.header,node: $(this).closest("li"),sourceName: col.value,sourceType: s}]);
+								};
+							}(val)));
 							last = last.css(conf).css({width: width,"padding-left":paddingleft+"px"}).addClass("jstree-grid-cell "+wcl+ " " + wideValClass + (tr?" ui-state-default":""));
 							
 							if (title) {
