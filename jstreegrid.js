e320480ef9ff5c87506b139cb7f46f75efc06bb9
Avi Deitcher <avi@deitcher.net>
2014-10-26 08:20:31 +0200

    Update version and comments

--- jstreegrid.js
+++ jstreegrid.js
@@ -6,10 +6,10 @@
  * Licensed under the MIT license:
  *   http://www.opensource.org/licenses/mit-license.php
  * 
- * Works only with jstree "v3.0.0-beta5" and higher
+ * Works only with jstree "v3.0.0" and higher
  *
- * $Date: 2014-04-18 $
- * $Revision:  3.1.1 $
+ * $Date: 2014-10-26 $
+ * $Revision:  3.1.2 $
  */
 
 /*jslint nomen:true */
