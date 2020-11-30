bc9f661e31472d5469d73e00b2695c590f8859e8
YarnSeemannsgarn <janschlenker1990@gmail.com>
2020-11-30 18:12:50 +0100

    Add CommonJS support

--- jstreegrid.js
+++ jstreegrid.js
@@ -23,6 +23,8 @@
 	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
 		define(['jquery', 'jstree'], factory);
+	} else if(typeof module !== 'undefined' && module.exports) {
+		module.exports = factory(require('jquery'));
 	} else {
 		// Browser globals
 		factory(jQuery);
